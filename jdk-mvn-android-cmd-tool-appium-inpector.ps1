c03db53a178cbfff8c3029d0529b1d4f30a148c3
testervippro <92687973+testervippro@users.noreply.github.com>
2025-04-18 21:12:05 +0700

    add install emulator

--- jdk-mvn-android-cmd-tool-appium-inpector.ps1
+++ jdk-mvn-android-cmd-tool-appium-inpector.ps1
@@ -369,3 +369,8 @@ Write-Host "SDK location: $androidHome"
 Write-Host " Android SDK setup completed successfully!"
 Write-Host " SDK location: $androidHome"
 
+# Install android device small version 
+sdkmanager "system-images;android-28;default;x86"
+avdmanager create avd --name "Mini_AVD_28" --package "system-images;android-28;default;x86" --device "Nexus 5" --force
+emulator -avd Mini_AVD_28
+
